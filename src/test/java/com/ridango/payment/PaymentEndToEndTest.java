a337b396c36f62d365fed72eda91712df5dc759f
Maksim Muruev <maksim.murujev@heathmont.net>
2021-01-04 10:10:05 +0200

    + added test coverage and enable validation

--- src/test/java/com/ridango/payment/PaymentEndToEndTest.java
+++ src/test/java/com/ridango/payment/PaymentEndToEndTest.java
@@ -41,8 +41,8 @@ public class PaymentEndToEndTest {
     @Test
     public void testTransactionFlow() throws Exception {
         // Initialize the database
-        Account senderUserAccount = new Account(null, "Test User", BigDecimal.valueOf(100));
-        Account receiverUserAccount = new Account(null, "Ridango User", BigDecimal.valueOf(100));
+        Account senderUserAccount = new Account(null, "Test@User", BigDecimal.valueOf(100));
+        Account receiverUserAccount = new Account(null, "Ridango@User", BigDecimal.valueOf(100));
 
         accountRepository.saveAll(List.of(senderUserAccount, receiverUserAccount));
         accountRepository.flush();
@@ -53,6 +53,7 @@ public class PaymentEndToEndTest {
         restMvc.perform(post("/payment")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsBytes(requestDTO)))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.transactionId").isNumber())
@@ -66,10 +67,10 @@ public class PaymentEndToEndTest {
     }
 
     @Test
-    public void testTransactionFlow_not_enouth_points() throws Exception {
+    public void testTransactionFlow_not_enough_points() throws Exception {
         // Initialize the database
-        Account senderUserAccount = new Account(null, "Test User 1", BigDecimal.valueOf(99));
-        Account receiverUserAccount = new Account(null, "Ridango User 1", BigDecimal.valueOf(100));
+        Account senderUserAccount = new Account(null, "Test+1@User", BigDecimal.valueOf(99));
+        Account receiverUserAccount = new Account(null, "Ridango+1@User", BigDecimal.valueOf(100));
 
         accountRepository.saveAll(List.of(senderUserAccount, receiverUserAccount));
         accountRepository.flush();
@@ -97,8 +98,8 @@ public class PaymentEndToEndTest {
     @Test
     public void testTransactionFlow_not_negative_points() throws Exception {
         // Initialize the database
-        Account senderUserAccount = new Account(null, "Test User 2", BigDecimal.valueOf(100));
-        Account receiverUserAccount = new Account(null, "Ridango User 2", BigDecimal.valueOf(100));
+        Account senderUserAccount = new Account(null, "Test+20@User", BigDecimal.valueOf(100));
+        Account receiverUserAccount = new Account(null, "Ridango+20@User", BigDecimal.valueOf(100));
 
         accountRepository.saveAll(List.of(senderUserAccount, receiverUserAccount));
         accountRepository.flush();
@@ -112,8 +113,104 @@ public class PaymentEndToEndTest {
                 .andDo(print())
                 .andExpect(status().is4xxClientError())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.type").value(equalTo("VALIDATION_ERROR")))
+                .andExpect(jsonPath("$.errors[0].code").value(equalTo("must be greater than or equal to 0")))
+                .andExpect(jsonPath("$.errors[0].field").value(equalTo("amount")))
+        ;
+
+        Account account = accountRepository.findById(senderUserAccount.getId()).orElseThrow();
+        assertThat(account.getBalance().longValue()).isEqualTo(100);
+
+        Account accountReceiver = accountRepository.findById(receiverUserAccount.getId()).orElseThrow();
+        assertThat(accountReceiver.getBalance().longValue()).isEqualTo(100);
+    }
+
+    @Test
+    public void testTransactionFlow_not_receiver() throws Exception {
+        // Initialize the database
+        Account senderUserAccount = new Account(null, "Test+3@User", BigDecimal.valueOf(100));
+
+        accountRepository.save(senderUserAccount);
+        accountRepository.flush();
+
+        PaymentRequestDTO requestDTO = new PaymentRequestDTO(senderUserAccount.getId(), null, BigDecimal.valueOf(100));
+
+        // Make transaction
+        restMvc.perform(post("/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsBytes(requestDTO)))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.type").value(equalTo("VALIDATION_ERROR")))
+                .andExpect(jsonPath("$.errors[0].code").value(equalTo("must not be null")))
+                .andExpect(jsonPath("$.errors[0].field").value(equalTo("receiverAccountId")))
+        ;
+
+        Account account = accountRepository.findById(senderUserAccount.getId()).orElseThrow();
+        assertThat(account.getBalance().longValue()).isEqualTo(100);
+    }
+
+    @Test
+    public void testTransactionFlow_no_sender_id() throws Exception {
+        PaymentRequestDTO requestDTO = new PaymentRequestDTO(-1L, -1L, BigDecimal.valueOf(100));
+
+        // Make transaction
+        restMvc.perform(post("/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsBytes(requestDTO)))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.type").value(equalTo("TRANSACTION_ERROR")))
-                .andExpect(jsonPath("$.errors[0].code").value(equalTo("ZERO_AMOUNT")))
+                .andExpect(jsonPath("$.errors[0].code").value(equalTo("NOT_FOUND_OWNER")))
+        ;
+
+    }
+
+    @Test
+    public void testTransactionFlow_no_receiver_id() throws Exception {
+        // Initialize the database
+        Account senderUserAccount = new Account(null, "Test+36@User", BigDecimal.valueOf(100));
+
+        accountRepository.save(senderUserAccount);
+        accountRepository.flush();
+
+        PaymentRequestDTO requestDTO = new PaymentRequestDTO(senderUserAccount.getId(), -1L, BigDecimal.valueOf(100));
+
+        // Make transaction
+        restMvc.perform(post("/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsBytes(requestDTO)))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.type").value(equalTo("TRANSACTION_ERROR")))
+                .andExpect(jsonPath("$.errors[0].code").value(equalTo("NOT_FOUND_OWNER")))
+        ;
+    }
+
+    @Test
+    public void testTransactionFlow_not_transaction_amount() throws Exception {
+        // Initialize the database
+        Account senderUserAccount = new Account(null, "Test+2@User", BigDecimal.valueOf(100));
+        Account receiverUserAccount = new Account(null, "Ridango+2@User", BigDecimal.valueOf(100));
+
+        accountRepository.saveAll(List.of(senderUserAccount, receiverUserAccount));
+        accountRepository.flush();
+
+        PaymentRequestDTO requestDTO = new PaymentRequestDTO(senderUserAccount.getId(), receiverUserAccount.getId(), null);
+
+        // Make transaction
+        restMvc.perform(post("/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsBytes(requestDTO)))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.type").value(equalTo("VALIDATION_ERROR")))
+                .andExpect(jsonPath("$.errors[0].code").value(equalTo("must not be null")))
+                .andExpect(jsonPath("$.errors[0].field").value(equalTo("amount")))
         ;
 
         Account account = accountRepository.findById(senderUserAccount.getId()).orElseThrow();
